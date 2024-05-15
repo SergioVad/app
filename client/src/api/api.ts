5dd62a13941f81e26b41c3b0d828b84dbdaddba4
Sergio <ivanov.sergey.vad@mail.ru>
2024-05-15 07:54:39 +0300

    change api

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const $api = axios.create({
-    baseURL: 'http://45.8.251.11:5000/api',
-    // baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://45.8.251.11:5000/api',
+    baseURL: 'http://localhost:5000/api',
 });
